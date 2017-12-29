a9ac7dcb167851343556cc6373cc1c37c1c89ca1
IIMuzolII <34108141+IIMuzolII@users.noreply.github.com>
2017-12-29 07:06:38 -0200

    Update and rename eight_level_solver.py to Anagramx.py

---
+++ 23 - Anagrama/Anagramx.py
@@ -0,0 +1,32 @@
+#The anagram file generete all the possibility of a word be mix
+#Last Update: 12/29/2017
+
+import random
+
+def factorial(x):
+    """
+        Generete the Factorial of the number passed.
+    """
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return x * fatorial(x - 1)
+
+word = input("Type the word here: ")
+len_word = len(word)
+anagrama = ''
+word_fact = factorial(len_word)
+
+for i in range(word_fact):
+    raffle = random.sample(range(len_word), len_word)
+
+    for i in raffle:
+        temp_int = int(i)
+        temp_letter = word[temp_int]
+        anagrama += temp_letter
+
+    if len(anagrama) == len_word:
+        print(anagrama)
+        with open('Anagrama.txt', 'a') as fl:
+            fl.write(anagrama+'\n')# Open a file 'Anagram' and write the word in it
+        anagrama = ''
